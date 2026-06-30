4b947291a7e174b1fe74dbf3960ce784f76a8acc
Luke Smith <smithl@tacc.utexas.edu>
2026-06-30 12:52:43 -0500

    tweaks to code unit normalization

--- src/MakeCloud/years_to_code_units.py
+++ src/MakeCloud/years_to_code_units.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 
 years = float(sys.argv[1])
-code_units = years*(3600*24*365.25)*((149597870700)/(math.tan(np.pi/648000)))**(-1)
+#code_units = years*(3600*24*365.25)*((149597870700)/(math.tan(np.pi/648000)))**(-1) #exact
+code_units = years*(3600*24*365.25)*1/(3.09e16)
 
 print(code_units)
